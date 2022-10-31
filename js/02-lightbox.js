703242395f27debfa311fe5b80d64bd98a9cc77b
yurii-kovalchuk <sogdrums@gmail.com>
2022-10-31 13:22:23 +0200

    done task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+
+let markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+  )
+  .join("");
+
+galleryRef.innerHTML = markup;
+
+new SimpleLightbox(".gallery a", { captionsData: "alt", captionDelay: 250 });
