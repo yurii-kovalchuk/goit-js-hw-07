bb33fd0bd47af953e799f0cde07ab9ebaf2ad923
yurii-kovalchuk <sogdrums@gmail.com>
2022-10-31 11:14:05 +0200

    done task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+let markup = galleryItems
+  .map(
+    ({ preview, description }) =>
+      `<div class="gallery__item"><img src="${preview}" alt="${description}" class="gallery__image"></div>`
+  )
+  .join("");
+
+galleryRef.innerHTML = markup;
+
+galleryRef.addEventListener("click", onClick);
+
+function onClick(e) {
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const currentItem = galleryItems.find(
+    ({ description }) => description === e.target.alt
+  );
+
+  basicLightbox
+    .create(
+      `<img src="${currentItem.original}" alt="${currentItem.description}">`
+    )
+    .show();
+}
